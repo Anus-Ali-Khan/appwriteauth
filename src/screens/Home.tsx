d31a4ed10acb05b6cbbd5743f80324ba4b5bcf5c
Anus Ali Khan <anusalikhan1405@gmail.com>
2024-02-10 12:28:21 +0500

    working on Home page

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,7 +1,31 @@
 import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import React, {useContext} from 'react';
+// reat native elements
+import {FAB} from '@rneui/themed';
+// Snackbar
+import Snackbar from 'react-native-snackbar';
+
+// context API
+import AppwriteContext from '../appwrite/AppwriteContext';
+
+type UserObj = {
+  name: String;
+  email: String;
+};
 
 const Home = () => {
+  const {appwrite, setIsLoggedIn} = useContext(AppwriteContext);
+
+  const handleLogout = () => {
+    appwrite.logout().then(() => {
+      setIsLoggedIn(false);
+      Snackbar.show({
+        text: 'Logout Successful',
+        duration: Snackbar.LENGTH_SHORT,
+      });
+    });
+  };
+
   return (
     <View>
       <Text>Home</Text>
