afc78a9bcf2aa0bfab2f9c73a09b6db5d8a1a719
Anus Ali Khan <anusalikhan1405@gmail.com>
2024-02-12 23:27:11 +0500

    working on Signup page

--- src/screens/Signup.tsx
+++ src/screens/Signup.tsx
@@ -1,5 +1,5 @@
 import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import React, {useContext, useState} from 'react';
 
 // reat native elements
 import {FAB} from '@rneui/themed';
@@ -15,6 +15,32 @@ import {AuthStackParamList} from '../routes/AuthStack';
 type SignupScreenProps = NativeStackScreenProps<AuthStackParamList, 'Signup'>;
 
 const Signup = () => {
+  const {appwrite, setIsLoggedIn} = useContext(AppwriteContext);
+  const [error, setError] = useState<string>('');
+  const [name, setName] = useState<string>('');
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [repeatPassword, setRepeatPassword] = useState<string>('');
+
+  const handleSignup = () => {
+    if (
+      name.length < 1 ||
+      email.length < 1 ||
+      password.length < 1 ||
+      repeatPassword.length < 1
+    ) {
+      setError('All fields are required');
+    } else if (password !== repeatPassword) {
+      setError('Passwords do not match');
+    } else {
+      const user = {
+        email,
+        password,
+        name,
+      };
+    }
+  };
+
   return (
     <View>
       <Text>Signup</Text>
