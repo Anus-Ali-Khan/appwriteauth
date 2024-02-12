09d88ca65122ccc8ae5ae8496dba08e2e2332196
Anus Ali Khan <anusalikhan1405@gmail.com>
2024-02-12 23:14:03 +0500

    Homepage completed

--- src/screens/Signup.tsx
+++ src/screens/Signup.tsx
@@ -1,6 +1,19 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 
+// reat native elements
+import {FAB} from '@rneui/themed';
+// Snackbar
+import Snackbar from 'react-native-snackbar';
+
+// context API
+import AppwriteContext from '../appwrite/AppwriteContext';
+// Navigation
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import {AuthStackParamList} from '../routes/AuthStack';
+
+type SignupScreenProps = NativeStackScreenProps<AuthStackParamList, 'Signup'>;
+
 const Signup = () => {
   return (
     <View>
